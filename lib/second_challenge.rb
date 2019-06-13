8dcef3d484687a51ac8326fa67de8cf2e0cbe2a9
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-13 00:06:04 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
+      :patriach => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       #your key/value pairs here!
       }, 
     :capulet => {
