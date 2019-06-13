cb3bd579ee8378ab4ffa0702e29ace8c6fa6f906
Jasmine Carter <jasmine.carter87@gmail.com>
2019-06-13 00:09:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,6 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
+        :name => "Lord Montague", :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
@@ -11,7 +12,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -26,5 +27,5 @@ def third_challenge
    }
   }
 
-  
+
 end
